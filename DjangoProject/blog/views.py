cd48d569b82651dc8ca525feaf9e1a8ca1b5d6be
jackkaikay <53180658+jackkaikay@users.noreply.github.com>
2020-03-06 16:34:47 +0000

    Post and posts added
    dummy data used

--- DjangoProject/blog/views.py
+++ DjangoProject/blog/views.py
@@ -2,9 +2,27 @@ from django.shortcuts import render
 from django.http import HttpResponse
 # Create your views here.
 
+
+posts = [
+    {
+        'author': 'jack',
+        'title': 'post 1',
+        'content': 'First post content',
+        'date_posted': '06/03/2020'
+    },
+{
+        'author': 'jack',
+        'title': 'post 2',
+        'content': 'Second post content',
+        'date_posted': '07/03/2020'
+    }
+]
 def home(request):
-    return HttpResponse('<h1> Blog Home </h1>')
+    context = {
+        'posts': posts
+    }
+    return render(request, 'blog/home.html',context)
 
 
 def about(request):
-    return HttpResponse('<h1> About </h1>')
+    return render(request, 'blog/about.html')
