3e75ea5d918a4e6f156d140f46346888b261937c
jackkaikay <53180658+jackkaikay@users.noreply.github.com>
2020-04-14 14:58:47 +0100

    p3 done

--- DjangoProject/blog/urls.py
+++ DjangoProject/blog/urls.py
@@ -2,6 +2,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.home , name='Blog-Home'),
-    path('about/', views.about , name='About-Home'),
+    path('', views.home , name='blog-home'),
+    path('about/', views.about , name='about-home'),
 ]
