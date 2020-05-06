8f922e42632796d97628732cd7e7d68ca09e8685
jackkaikay <53180658+jackkaikay@users.noreply.github.com>
2020-05-06 14:25:45 +0100

    added working login / logout system

--- DjangoProject/users/views.py
+++ DjangoProject/users/views.py
@@ -8,13 +8,18 @@ def register(request):
         if form.is_valid():
             form.save()
             username = form.cleaned_data.get('username')
-            messages.success(request, f'Account created for {username}!')
-            return redirect('blog-home')
+            messages.success(request, f'{username} account has been created! You are now able to login!')
+            return redirect('login')
 
 
     else:
         form = UserRegisterForm()
     return render(request, 'users/register.html', {'form': form })
 
+
+
+
+def profile(request):
+    return render(request, 'users/profile.html')
 
 
