82d3abac16ade5e808f3547fc3fa59df820858ed
jackkaikay <53180658+jackkaikay@users.noreply.github.com>
2020-05-14 14:39:09 +0100

    Blog posts and some vaidation added

--- DjangoProject/blog/views.py
+++ DjangoProject/blog/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 from .models import Post
+from django.views.generic import ListView, DetailView, CreateView
 # Create your views here.
 
 
@@ -11,6 +12,22 @@ def home(request):
     }
     return render(request, 'blog/home.html',context)
 
+class PostListView(ListView):
+    model = Post
+    template_name = 'blog/home.html'
+    context_object_name = 'posts'
+    ordering = ['-date_posted']
+
+class PostDetailView(DetailView):
+    model = Post
+
+class PostCreateView(CreateView):
+    model = Post
+    fields = ['title', 'content']
+
+    def form_valid(self, form):
+        form.instance.author = self.request.user
+        return super().form_valid(form)
 
 def about(request):
     return render(request, 'blog/about.html', {'title': 'About'})
