d9aaa35f7e548f5c7f89e358b7a57822addb6662
jackkaikay <53180658+jackkaikay@users.noreply.github.com>
2020-05-15 13:56:33 +0100

    added validation to blogupdates

--- DjangoProject/blog/views.py
+++ DjangoProject/blog/views.py
@@ -1,7 +1,8 @@
 from django.shortcuts import render
+from django.contrib.auth.mixins import LoginRequiredMixin, UserPassesTestMixin
 from django.http import HttpResponse
 from .models import Post
-from django.views.generic import ListView, DetailView, CreateView
+from django.views.generic import ListView, DetailView, CreateView, UpdateView
 # Create your views here.
 
 
@@ -21,13 +22,31 @@ class PostListView(ListView):
 class PostDetailView(DetailView):
     model = Post
 
-class PostCreateView(CreateView):
+class PostCreateView(LoginRequiredMixin, CreateView):
     model = Post
     fields = ['title', 'content']
 
     def form_valid(self, form):
         form.instance.author = self.request.user
         return super().form_valid(form)
+
+
+class PostUpdateView(LoginRequiredMixin, UserPassesTestMixin, UpdateView):
+    model = Post
+    fields = ['title', 'content']
+
+    def form_valid(self, form):
+        form.instance.author = self.request.user
+        return super().form_valid(form)
+
+    def test_func(self):
+        post = self.get_object()
+        if self.request.user == post.author:
+            return True
+        return False
+
+
+
 
 def about(request):
     return render(request, 'blog/about.html', {'title': 'About'})
